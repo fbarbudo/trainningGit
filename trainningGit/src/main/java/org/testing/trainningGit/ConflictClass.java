43d95e4010316e129a678f19b5e71e102616bdb5
paco <paco.barbudo@gmail.com>
2013-07-07 03:51:36 +0200

    anadir string tarea2 mas gets and sets

--- trainningGit/src/main/java/org/testing/trainningGit/ConflictClass.java
+++ trainningGit/src/main/java/org/testing/trainningGit/ConflictClass.java
@@ -2,4 +2,14 @@ package org.testing.trainningGit;
 
 public class ConflictClass {
 
+	private String tarea2;
+
+	public String getTarea2() {
+		return tarea2;
+	}
+
+	public void setTarea2(String tarea2) {
+		this.tarea2 = tarea2;
+	}
+	
 }
