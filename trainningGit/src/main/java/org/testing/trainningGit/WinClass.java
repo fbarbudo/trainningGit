2adcf944d3c12a1a1667af3a03a2faceb97055d9
paco <paco.barbudo@gmail.com>
2013-07-09 00:59:14 +0200

    first commit

---
+++ trainningGit/src/main/java/org/testing/trainningGit/WinClass.java
@@ -0,0 +1,15 @@
+package org.testing.trainningGit;
+
+public class WinClass {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
